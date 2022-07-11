0145946fd4b37f2b0f20c1232fd3e583c0ac545a
jhyan <jhyan@cs.utah.edu>
2022-07-11 00:36:56 -0600

    Jul11-00:36:54-MDT2022

--- run.sh
+++ run.sh
@@ -1,6 +1,27 @@
+pref="/uufs/chpc.utah.edu/common/home/u1320844/GNN_logs/"
 dt=$(date | awk '{print$2$3"-"$4"-"$5$6}')
+
 echo "Date: " $dt
-env_output=$dt"_env_info.txt"
-prog_output=$dt"_running_log.txt"
+
+git_output=$pref$dt"_git_info.txt"
+env_output=$pref$dt"_env_info.txt"
+prog_output=$pref$dt"_running_log.txt"
+
 echo $env_output
 echo $prog_output
+
+git show > $git_output
+git diff > $git_output
+env > $env_output
+pip list > $env_output
+
+cmd=$1
+echo $cmd
+git add .
+git commit -m $dt
+git push origin master
+pushd ../GNN_logs
+git add .
+git commit -m $dt
+git push origin master
+popd
