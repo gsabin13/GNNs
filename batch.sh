8a4335ef34706e62026b7b6cb85ddbd326e38218
jhyan <jhyan@cs.utah.edu>
2022-07-17 18:06:01 -0600

    Jul17-18:05:55-MDT2022

--- batch.sh
+++ batch.sh
@@ -23,5 +23,6 @@ conda activate ogb
 module load gcc/9.2.0
 module load cuda/11.0
 
-sh run.sh sh dgl/run_dgl.sh full_new.csv
+sh run.sh sleep 1s
+#sh run.sh sh dgl/run_dgl.sh full_new.csv
 #sh run.sh sh GNN-RDM/genacc_rdm.sh 
